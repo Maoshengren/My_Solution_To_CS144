82cb3d0aa2904d12bcad16c08247f3ceee1c5fbf
Maoshengren <1379338142@qq.com>
2022-02-22 19:04:37 +0800

    finish lab1

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,5 +1,8 @@
 #include "stream_reassembler.hh"
 
+#include <iostream>
+#include <algorithm>
+
 // Dummy implementation of a stream reassembler.
 
 // For Lab 1, please replace with a real implementation that passes the
@@ -7,20 +10,57 @@
 
 // You will need to add private members to the class declaration in `stream_reassembler.hh`
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+//template <typename... Targs>
+//void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : buff(capacity, '\0'), check(capacity, false), output(capacity), myCapacity(capacity) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    size_t size = data.size();
+    if(index >= headIndex + output.remaining_capacity())
+        return;
+    if(index + size < headIndex){
+        if(eof)
+            isEof = true;
+    }
+    else {
+        if (index + data.size() <= headIndex + output.remaining_capacity()) 
+            if(eof)
+                isEof = true;
+        size_t len = min(index + data.size(), headIndex + output.remaining_capacity());
+        for(size_t i = index; i < len; i++){ 
+            if(i >= headIndex && !check[i - headIndex]){
+                buff[i - headIndex] = data[i - index];
+                check[i - headIndex] = true;
+                unassembledBytes++;
+            }
+        }
+        string str = "";
+        while (check.front()) {
+            str += buff.front();
+            buff.pop_front();
+            buff.push_back('\0');
+            check.pop_front();
+            check.push_back(false);
+        }
+
+        len = str.size();
+        if (len > 0) {
+            unassembledBytes -= len;
+            headIndex += len;
+            output.write(str);
+        }
+    }
+    if (isEof && empty()) {
+        output.end_input();
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const { return unassembledBytes; }
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return unassembledBytes == 0; }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -3,17 +3,32 @@
 
 #include "byte_stream.hh"
 
+#include <algorithm>
 #include <cstdint>
+#include <iostream>
+#include <list>
+#include <set>
+#include <stdexcept>
 #include <string>
-
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
-
-    ByteStream _output;  //!< The reassembled in-order byte stream
-    size_t _capacity;    //!< The maximum number of bytes
+    bool isEof = false;
+    size_t headIndex = 0;
+    size_t unassembledBytes = 0;
+    std::deque<char> buff;
+    std::deque<bool> check;
+    ByteStream output;
+    size_t myCapacity = 0;
+    // std::deque<char> _buf;
+    // std::deque<bool> _check;
+    // size_t _unassembled_bytes {0};
+    // size_t _head_index {0};
+    // bool _is_eof {false};
+    // ByteStream _output;  //!< The reassembled in-order byte stream
+    // size_t _capacity;    //!< The maximum number of bytes
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
@@ -21,31 +36,36 @@ class StreamReassembler {
     //! and those that have not yet been reassembled.
     StreamReassembler(const size_t capacity);
 
-    //! \brief Receive a substring and write any newly contiguous bytes into the stream.
+    //! \brief Receives a substring and writes any newly contiguous bytes into the stream.
     //!
-    //! The StreamReassembler will stay within the memory limits of the `capacity`.
-    //! Bytes that would exceed the capacity are silently discarded.
+    //! If accepting all the data would overflow the `capacity` of this
+    //! `StreamReassembler`, then only the part of the data that fits will be
+    //! accepted. If the substring is only partially accepted, then the `eof`
+    //! will be disregarded.
     //!
-    //! \param data the substring
-    //! \param index indicates the index (place in sequence) of the first byte in `data`
-    //! \param eof the last byte of `data` will be the last byte in the entire stream
+    //! \param data the string being added
+    //! \param index the index of the first byte in `data`
+    //! \param eof whether or not this segment ends with the end of the stream
     void push_substring(const std::string &data, const uint64_t index, const bool eof);
 
     //! \name Access the reassembled byte stream
     //!@{
-    const ByteStream &stream_out() const { return _output; }
-    ByteStream &stream_out() { return _output; }
+    const ByteStream &stream_out() const { return output; }
+    ByteStream &stream_out() { return output; }
     //!@}
 
     //! The number of bytes in the substrings stored but not yet reassembled
     //!
-    //! \note If the byte at a particular index has been pushed more than once, it
+    //! \note If the byte at a particular index has been submitted twice, it
     //! should only be counted once for the purpose of this function.
     size_t unassembled_bytes() const;
 
     //! \brief Is the internal state empty (other than the output stream)?
     //! \returns `true` if no substrings are waiting to be assembled
     bool empty() const;
+
+    size_t head_index() const { return headIndex; }
+    bool input_ended() const { return output.input_ended(); }
 };
 
 #endif  // SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
